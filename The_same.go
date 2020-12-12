974a8da29300a3d289c0c620a841dac24a6b2227
Leo <40925126+Botiyava@users.noreply.github.com>
2020-12-12 13:44:38 +0300

    6 kuy

--- The_same.go
+++ The_same.go
@@ -4,7 +4,6 @@
  *that checks whether the two arrays have the "same" elements, with the same multiplicities.
  *"Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
  *https://www.codewars.com/kata/550498447451fbbd7600041c
- *
  */
 package kata
 
