ab944f28b9fde216199612b0b79bbaa874afe224
Leo <40925126+Botiyava@users.noreply.github.com>
2020-12-12 13:40:56 +0300

    Create Deadfish.go
    6 kuy

---
+++ Deadfish.go
@@ -0,0 +1,38 @@
+/*
+ *Write a simple parser that will parse and run Deadfish.
+ *
+ *Deadfish has 4 commands, each 1 character long:
+ *
+ *i increments the value (initially 0)
+ *d decrements the value
+ *s squares the value
+ *o outputs the value into the return array
+ *Invalid characters should be ignored.
+ */
+package kata
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Parse(data string) []int{
+buf := 0
+slice := []int{}
+str := strings.Split(data, "")
+for i := 0; i < len(str); i++{
+	switch str[i]{
+	case "i":
+		buf += 1
+	case "d":
+		buf -= 1
+	case "s":
+		buf *= buf
+	case "o":
+
+		slice = append(slice, buf)
+	}
+}
+	
+return slice
+}
