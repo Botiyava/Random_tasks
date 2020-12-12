18e8b5b325ef4b6efbac267ce86d9a6c86a3a3cc
Leo <40925126+Botiyava@users.noreply.github.com>
2020-12-12 13:45:00 +0300

    6 kuy

--- Deadfish.go
+++ Deadfish.go
@@ -3,7 +3,6 @@ https://www.codewars.com/kata/51e0007c1f9378fa810002a9/train/go
  *Write a simple parser that will parse and run Deadfish.
  *
  *Deadfish has 4 commands, each 1 character long:
- *
  *i increments the value (initially 0)
  *d decrements the value
  *s squares the value
