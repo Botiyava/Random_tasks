31c99135fc2c2d913159e6ee0d4f6138e739c963
Leonid <40925126+Botiyava@users.noreply.github.com>
2021-06-01 00:57:00 +0300

    6 kyu

--- books_categories.go
+++ books_categories.go
@@ -1,10 +1,12 @@
 /*
+https://www.codewars.com/kata/54dc6f5a224c26032800005c
 A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. 
 Each book has a code c of 3, 4, 5 or more characters. The  characters of a code is a capital letters which defines the book category.
 
 In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) 
 which indicates the quantity of books of this code in stock.
 Your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category. 
+If L or M are empty return string is "" (Clojure and Racket should return an empty array/list instead).
 */
 
 package main
@@ -15,7 +17,11 @@ import (
 	"strings"
 )
 
+
 func StockList(listArt []string, listCat []string) string {
+	if len(listArt) == 0 || len(listCat) == 0{
+		return ""
+	}
 result := map[string]int{}
 for i := 0; i < len(listArt); i++{
 	var quantityString string
@@ -32,6 +38,9 @@ for i := 0; i < len(listArt); i++{
 	}
 	for _, elem := range currentString{
 		result[elem] += quantity
+		break //Костыль. Если его убрать, то программа будет считать все заглавные буквы как
+		      //категорию для книги, а не только первую. Сделано из-за моей невнимательности.
+		//По сути фиксится за 2 минуты, но уже час ночи, так что всем спокойной ночи :)
 	}
 
 }
@@ -46,11 +55,4 @@ for i, elem := range listCat{
 }
 
 return str
-}
-func main() {
-	L := []string{"ABART 2000", "CDXEF 5000", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}
-	M := []string{"A", "B", "C", "W"}
-	fmt.Println(StockList(L, M))
-
-
 }
