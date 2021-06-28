e47bd47626d59d77f426a209a9ce6a12129bedf3
Leonid <40925126+Botiyava@users.noreply.github.com>
2021-06-28 18:31:40 +0300

    Create main.go

---
+++ Tests/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"awesomeProject7/ops"
+	"fmt"
+)
+
+func main() {
+
+	op := ops.GetKeyOperator()
+
+	key := op.Generate(2, 3)
+
+	a, b, _ := op.Degenerate(key)
+
+	fmt.Printf("key=%v, a=%v, b=%v", key, a, b)
+
+}
